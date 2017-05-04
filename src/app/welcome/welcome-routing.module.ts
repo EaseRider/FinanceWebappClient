4326cde36dd28797dc1c33eaefb8da6a9263b809
jengeler <jengeler@hsr.ch>
2017-05-04 08:26:55 +0200

    moved guard to dashboard;

--- src/app/welcome/welcome-routing.module.ts
+++ src/app/welcome/welcome-routing.module.ts
@@ -3,16 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 
 import {LoginComponent, RegisterComponent} from "../auth/components";
 import {WelcomeComponent} from "./welcome.component";
-import {AuthGuardLogin} from "../auth/services/authguard-login.service";
 
 const appRoutes: Routes = [
   {
     path: 'welcome',
     component: WelcomeComponent,
-    // TODO: Add guard and routing (Register/Login) here...
     children: [
-      { path: '', component: LoginComponent, canActivate: [AuthGuardLogin] },
-      { path: 'register', component: RegisterComponent, canActivate: [AuthGuardLogin] }
+      { path: '', component: LoginComponent },
+      {
+        path: 'register',
+        component: RegisterComponent
+      }
     ]
   }
 ];
